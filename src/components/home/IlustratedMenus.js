a2c8ebd9a01cb41f10d1d0917a6fb8d8a86c3d04
Marcelo Basso <bassomarcelo.ba@gmail.com>
2023-05-21 12:47:03 -0300

    :ambulance: remove texto duplicado

--- src/components/home/IlustratedMenus.js
+++ src/components/home/IlustratedMenus.js
@@ -43,7 +43,6 @@ export default function IlustratedMenus() {
                         </div>
                     </div>
                     <div className="col-lg-7 col-md-12 d-flex flex-column justify-content-center">
-                        <p>Criados para facilitar o entendimento do plano alimentar e possibilitar trocas e maior flexibilidade alimentar.</p>
                         <p>Meu objetivo é te <b>ensinar a comer</b>, sem restrições alimentares, e em qualquer lugar em que você estiver. Os cardápios ilustrados permitem maior liberdade nas suas escolhas, afinal, o plano alimentar é <span className="green-underline">para você</span>. Por isso, para uma aderência maior, é importante que você participe ativamente da criação do seu plano, e que, entenda como fazer possíveis trocas no seu dia a dia!</p>
                         <p><b>Esse método foi desenvolvido por mim</b>, ao perceber a dificuldade das pessoas em aderir a um plano alimentar sempre igual. Geralmente é o que você ganha quando vai na nutricionista: um plano calculado para um dia. Mas o seu dia a dia muda, e as situações sociais a que você é exposto também. O mais importante é que você aprenda como combinar os alimentos, sem medo de comer, e sem terrorismo alimentar.</p>
                         <p>Por isso, com meu método <b className="green-underline">você conseguirá se alimentar de forma equilibrada e sem restrições desnecessárias!</b></p>
